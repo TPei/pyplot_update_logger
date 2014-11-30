495de5255afb612e0c277939c855d646f92b02cd
Thomas Peikert <t.s.peikert@gmail.com>
2014-11-30 08:46:02 +0100

    some minor adjustments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 pyplot_update_logger
 ====================
 
-lib for easy updating of pyplots when new data is received
+auto updating pyplots
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 __author__ = 'TPei'
 import matplotlib.pyplot as plt
-import numpy as np
-import random
 from timeit import default_timer as timer
 
 x = []
@@ -16,11 +14,18 @@ line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
 start = timer()
 for i in range(1, 1000000000):
     if i % 1000 == 0:
+        # just some time measurements as data
         x.append(i)
         y.append(timer() - start)
         start = timer()
+
+        # set new data
         line1.set_xdata(x)
         line1.set_ydata(y)
+
+        # resetting the limits is necessary when the graph max / min changes
         plt.ylim(min(y[-1000:]), max(y[-1000:]))
         plt.xlim(min(x[-1000:]), max(x))
+
+        # redraw
         fig.canvas.draw()
